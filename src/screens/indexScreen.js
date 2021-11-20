f2e90ef848acd712d4130743a970be320a6bea4c
Chris Rebuck <rebuckc2308@gmail.com>
2021-11-19 23:25:59 -0600

    Created a createDataContext function incase we ever wanted to add another resource to application we can use this to help manage changes to state and pass information between parent and nested child components

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,15 +1,15 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
-import BlogContext from "../context/BlogContext";
+import { Context } from "../context/BlogContext";
 
 const IndexScreen = () => {
-  const { data, addBlogpost } = useContext(BlogContext);
+  const { state, addBlogpost } = useContext(Context);
   return (
     <View>
       <Text>Index Screen</Text>
       <Button title="Add Blog Post" onPress={addBlogpost} />
       <FlatList
-        data={data}
+        data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
           return <Text>{item.title}</Text>;
