2e0a4821a7cc149dbdc97b5985a0027aeed899f3
Chris Rebuck <rebuckc2308@gmail.com>
2021-11-21 10:49:56 -0600

    created a screen to edit blogposts and ability to navigate from the ShowScreen

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,10 @@
+import React, { useContext } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const EditScreen = () => {
+  return <Text>Edit Blogpost</Text>;
+};
+
+const style = StyleSheet.create({});
+
+export default EditScreen;
