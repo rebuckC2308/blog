fdd9d322cc6f87424dc4003ec381e40cc07511b3
Chris Rebuck <rebuckc2308@gmail.com>
2021-11-23 14:33:49 -0600

    fixed bug

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import IndexScreen from "./src/screens/IndexScreen";
 import { Provider } from "./src/context/BlogContext";
+import IndexScreen from "./src/screens/indexScreen";
 import ShowScreen from "./src/screens/ShowScreen";
 import CreateScreen from "./src/screens/CreateScreen";
 import EditScreen from "./src/screens/EditScreen";
